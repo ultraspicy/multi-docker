db521257d17f68a824845052ad55edd71dd56de0
ultraspicy <element.no.88@gmail.com>
2023-05-27 23:33:07 -0700

    Minor change on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, today is Saturday. Edit <code>src/App.js</code> and save to reload.
+          Hello, today is Sunday. Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
