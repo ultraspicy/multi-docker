92b3df4098a4c7635b8231e9fc27e5bd9c994465
ultraspicy <element.no.88@gmail.com>
2023-05-27 19:13:23 -0700

    Checkout files

--- complex/server/keys.js
+++ complex/server/keys.js
@@ -1,11 +1,9 @@
 module.exports = {
-    // environment variable 
-    redisHost: process.env.REDIS_HOST,
-    redisPort: process.env.REDIS_PORT,
-
-    pgUser: process.env.PGUSER,
-    pgHost: process.env.PGHOST,
-    pgDatabase: process.env.PGDATABASE,
-    pgPassword: process.env.PGPASSWORD,
-    pgPort: process.env.PGPORT
-  };
+  redisHost: process.env.REDIS_HOST,
+  redisPort: process.env.REDIS_PORT,
+  pgUser: process.env.PGUSER,
+  pgHost: process.env.PGHOST,
+  pgDatabase: process.env.PGDATABASE,
+  pgPassword: process.env.PGPASSWORD,
+  pgPort: process.env.PGPORT,
+};
