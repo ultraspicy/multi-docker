42fb488bb3bb42f0fa564defb35a0c262913d523
ultraspicy <element.no.88@gmail.com>
2023-05-27 20:19:03 -0700

    Add gitignore in sub directories to skip node_module folders

--- visits/index.js
+++ visits/index.js
@@ -13,7 +13,7 @@ client.set('visits', 0);
 
 app.get('/', (req, res) => {
     client.get('visits', (err, visits) => {
-        process.exit(0);
+        //process.exit(0);
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
     });
